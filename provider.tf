2ad01a1bbe284d45a6d3c989e1d71a437eaeb360
sra1sravan <102434717+sra1sravan@users.noreply.github.com>
2022-04-23 17:30:45 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA5JMELCDRORKJRK4F"
-  secret_key = "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"
+  access_key = "AKIA5JMELCDRINOZO3FF"
+  secret_key = "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"
 }
